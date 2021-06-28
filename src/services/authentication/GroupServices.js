9338db61b7e4a04d330311b6efc903546a1f6af9
Subhadip Sahoo <subhadip.sahoo@tier5.in>
2021-06-28 23:33:15 +0530

    FEATURE/GROUP: Implemented group sorting functionality

--- src/services/authentication/GroupServices.js
+++ src/services/authentication/GroupServices.js
@@ -9,25 +9,18 @@ let headers = {
 export const GroupServices = {
     fetchGroups: async (page = null, queryParams = null) => {
         headers.Authorization = localStorage.getItem("_token");
-        return new Promise((resolve, reject) => {
-            axios
-                .get(
-                    config.groupUrl + 'list' + 
-                    (page ? "/" + page : '') + 
-                    (queryParams ? "?" + queryParams : ''),
-                    { headers: headers }
-                )
-                .then((result) => {
-                    console.log('Fetch groups services result: ', result);
-                    resolve(result.data);
-                })
-                .catch((error) => {
-                    console.log('Fetch groups service error: ', error);
-                    if (error != null) {
-                        reject(error);
-                    }
-                });
-        });
+        try {
+            const result = await axios.get(
+                config.groupUrl + 'list' + 
+                (page ? "/" + page : '') + 
+                (queryParams ? "?" + queryParams : ''),
+                { headers: headers });
+            console.log("fetchGroups() Service result", result);
+            return result.data;
+        } catch (e) {
+            console.log("fetchGroups() service error result", e);
+            throw new Error(e);
+        }
     },
     createGroup: async (payload) => {
         headers.Authorization = localStorage.getItem("_token");
@@ -47,8 +40,8 @@ export const GroupServices = {
             console.log('Delete group service : ', result);
             return result.data;
         } catch (e) {
-            console.log('Delete group service error: ', e);
-            return e;
+            console.log('Delete Group Service Catch Block : ', e);
+            throw new Error(e);
         }
     }
 
