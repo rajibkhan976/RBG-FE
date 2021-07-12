fc4d058f49fc327ee1d952050702299e9daa048f
amar-nath-das <amarnath.das@tier5.in>
2021-07-12 13:50:27 +0530

    Contact Module restructuring ui v1.0.0

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,33 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import LeftMenu from "../shared/LeftMenu";
-import HeaderDashboard from "../shared/HeaderDashboard";
-import DashboardFooter from "../shared/FooterDashboard";
-import InnerLeftMenu from "../shared/InnerLeftMenu";
-import ImportContactModal from './importContactModal';
+import ImportContactModal from "./importContactModal";
 
 const contact = () => {
+  return (
+    <>
+      <div className="dashInnerUI"></div>
+      <ImportContactModal />
+    </>
+  );
+};
 
-    
-
-    return (
-        <div className="mainComponent">
-      <div className="dashboardBody d-flex f-align-center">
-        <LeftMenu />
-        <div className="dashMain">
-          <InnerLeftMenu />
-          <div className="dashboardElComponent">
-            <HeaderDashboard  />
-            <div className="dashInnerStructure">
-              <h1></h1>
-                <ImportContactModal />
-              <DashboardFooter />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    )
-}
-
-export default contact
+export default contact;
