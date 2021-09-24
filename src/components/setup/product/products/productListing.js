9e0d3b91db72858bb8b6bff432979f6d4917ed17
ishitapaulroy <ishita.paulroy@tier5.in>
2021-09-24 17:37:37 +0530

    productlisting new design implementation

--- src/components/setup/product/products/productListing.js
+++ src/components/setup/product/products/productListing.js
@@ -5,7 +5,7 @@ import AddProductModal from "./addProductModal";
 import plus_icon from "../../../../assets/images/plus_icon.svg";
 import percentTag from "../../../../assets/images/percentage_icon.png";
 import list_board_icon from "../../../../assets/images/list_board_icon.svg";
-// import proImg1 from "../../../../assets/images/proImg1.png";
+// import proImg1 from "../../../../assets/images/proImg1.png";   
 // import proImg2 from "../../../../assets/images/proImg2.png";
 // import proImg3 from "../../../../assets/images/proImg3.png";
 // import proImg4 from "../../../../assets/images/proImg4.png";
@@ -13,6 +13,8 @@ import list_board_icon from "../../../../assets/images/list_board_icon.svg";
 // import proImg6 from "../../../../assets/images/proImg6.png";
 import proImg7 from "../../../../assets/images/proImg7.png";
 import listView from "../../../../assets/images/listView.svg";
+import dot3White from "../../../../assets/images/dot3gray.svg";
+
 import CategoryListing from "./categories";
 import { ErrorAlert, SuccessAlert } from "../../../shared/messages";
 import { utils } from "../../../../helpers";
@@ -20,6 +22,7 @@ import Loader from "../../../shared/Loader";
 import env from "../../../../configuration/env";
 import { ProductServices } from "../../../../services/setup/ProductServices";
 import Pagination from "../../../shared/Pagination";
+
 
 const ProductListing = () => {
     document.title = "Products";
@@ -147,11 +150,60 @@ const ProductListing = () => {
                                         <div className="productListRight">
                                             <div className="chooseSize">
                                                 <p>Size</p>
+                                                <span>S</span>
                                                 {elem.size.map(s => <span>{s}</span>)}
+                                                <span>XL</span>
+                                                <span>2XL</span>
+                                                <span>3XL</span>
                                             </div>
                                             <div className="chooseColor">
                                                 <p>Color</p>
+                                                <span style={{ backgroundColor: "#ABED93" }}></span>
                                                 {elem.colors.map(c => <span className={c}></span>)}
+                                                <div className="colorpaletContainer">
+                                                    <button className="dropIt">+12</button>
+                                                    <div className="colorPalet paletHide"> {/*//paletHide class is to be added to hide it */}
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                        <span style={{ backgroundColor: "#ABED93" }}></span>
+                                                    </div> 
+                                                </div>
+                                                
+                                            </div>
+                                            <div className="sideEditOption">
+                                                <button className="showList" >
+                                                <img src={dot3White} alt="" />
+                                                </button>
+                                                 <div class="dropdownOptions listOpen"> {/*//listHide class is to be replaced with listOpen to hide it */}
+                                                    <button class="btn btnEdit">
+                                                        <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.553 13.553" class="editIcon"><g transform="translate(0.75 0.75)">
+                                                            <path class="a" d="M12.847,10.424v3.218a1.205,1.205,0,0,1-1.205,1.205H3.205A1.205,1.205,0,0,1,2,13.642V5.205A1.205,1.205,0,0,1,3.205,4H6.423" transform="translate(-2 -2.795)"></path>
+                                                            <path class="a" d="M14.026,2l2.411,2.411-6.026,6.026H8V8.026Z" transform="translate(-4.384 -2)"></path></g>
+                                                             </svg>
+                                                        </span> Edit
+                                                    </button>
+                                                    <button class="btn btnDelete">
+                                                       <span>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="12.347" height="13.553" viewBox="0 0 12.347 13.553" class="deleteIcon">
+                                                            <g transform="translate(0.75 0.75)"><path class="a" transform="translate(-3 -3.589)"></path>
+                                                                <path class="a" d="M13.437,4.411v8.437a1.205,1.205,0,0,1-1.205,1.205H6.205A1.205,1.205,0,0,1,5,12.847V4.411m1.808,0V3.205A1.205,1.205,0,0,1,8.013,2h2.411a1.205,1.205,0,0,1,1.205,1.205V4.411" transform="translate(-3.795 -2)"></path><line class="a" y2="3" transform="translate(4.397 6.113)"></line><line class="a" y2="3" transform="translate(6.397 6.113)">
+
+                                                                </line></g></svg>
+                                                        </span> Delete
+                                                    </button>
+                                                </div>           
                                             </div>
                                         </div>
                                     </div>
