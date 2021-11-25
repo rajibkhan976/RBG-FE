b0a535db544659f2f362c1aadc1c5ab9af552057
amar-nath-das <amarnath.das@tier5.in>
2021-11-25 12:22:17 +0530

    Setup menu text v1.0.0

--- src/components/setup/mainPopup/setup.js
+++ src/components/setup/mainPopup/setup.js
@@ -9,143 +9,160 @@ import SetupIcon5 from "../../../assets/images/setupicon5.svg";
 import SetupIcon6 from "../../../assets/images/setupicon6.svg";
 import SetupIcon7 from "../../../assets/images/setupicon7.svg";
 
-
 const Setup = (props) => {
-    return (
-        <div className="">
-            <div className="setUpPopUp">
-                <p className="headding1">Set Up </p>
-                <ul>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon1} alt="" /></i>
-                            <div>
-                                <h3>Personal Details</h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon2} alt="" /></i>
-                            <div>
-                                <h3>Gym Details</h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon3} alt="" /></i>
-                            <div>
-                                <h3>Communication Setup</h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                        <ul className="secondListing">
-                            <li>
-                                <NavLink to="/email-setup">
-                                    Email
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/sms-setup">
-                                    SMS
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/call-setup">
-                                    Call
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon4} alt="" /></i>
-                            <div>
-                                <h3>Data Administration</h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                        <ul className="secondListing">
-                            <li><a href="#">Import History</a></li>
-                            <li><a href="#">Export History</a></li>
-                            <li><a href="#">Activity Log</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon5} alt="" /></i>
-                            <div>
-                                <h3>
-                                    <NavLink to="/products">
-                                        Products
-                                    </NavLink>
-                                </h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon6} alt="" /></i>
-                            <div>
-                                <h3>Customizations</h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon7} alt="" /></i>
-                            <div>
-                                <h3>Templates</h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                        <ul className="secondListing">
-                            <li><a href="#">Email</a></li>
-                            <li><a href="#">SMS</a></li>
-                            <li>
-                                <NavLink to="/audio-template">
-                                    Audio
-                                </NavLink>
-                            </li>
-                            <li><a href="#">RVM</a></li>
-                            <li><a href="#">Sales Bridge</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon4} alt="" /></i>
-                            <div>
-                                <h3>
-                                    <NavLink to="/number-list">
-                                        Number List
-                                    </NavLink>
-                                </h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listHead">
-                            <i><img src={SetupIcon5} alt="" /></i>
-                            <div>
-                                <h3>
-                                    <NavLink to="/courses">
-                                        Courses
-                                    </NavLink>
-                                </h3>
-                                <p>Lorem ipsum dolor sit</p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+  return (
+    <div className="">
+      <div className="setUpPopUp">
+        <p className="headding1">Set Up </p>
+        <ul>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon1} alt="" />
+              </i>
+              <div>
+                <h3>Personal Details</h3>
+                <p>Manage your personal details</p>
+              </div>
             </div>
-        </div>
-    );
-}
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon2} alt="" />
+              </i>
+              <div>
+                <h3>Gym Details</h3>
+                <p>Manage your Gym details</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon3} alt="" />
+              </i>
+              <div>
+                <h3>Communication Setup</h3>
+                <p>Set all kind of communications</p>
+              </div>
+            </div>
+            <ul className="secondListing">
+              <li>
+                <NavLink to="/email-setup">Email</NavLink>
+              </li>
+              <li>
+                <NavLink to="/sms-setup">SMS</NavLink>
+              </li>
+              <li>
+                <NavLink to="/call-setup">Call</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon4} alt="" />
+              </i>
+              <div>
+                <h3>Data Administration</h3>
+                <p>Lorem ipsum dolor sit</p>
+              </div>
+            </div>
+            <ul className="secondListing">
+              <li>
+                <a href="#">Import History</a>
+              </li>
+              <li>
+                <a href="#">Export History</a>
+              </li>
+              <li>
+                <a href="#">Activity Log</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon5} alt="" />
+              </i>
+              <div>
+                <h3>
+                  <NavLink to="/products">Products</NavLink>
+                </h3>
+                <p>Manage your POS products</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon6} alt="" />
+              </i>
+              <div>
+                <h3>Customizations</h3>
+                <p>Lorem ipsum dolor sit</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon7} alt="" />
+              </i>
+              <div>
+                <h3>Templates</h3>
+                <p>Set communication templates</p>
+              </div>
+            </div>
+            <ul className="secondListing">
+              <li>
+                <a href="#">Email</a>
+              </li>
+              <li>
+                <a href="#">SMS</a>
+              </li>
+              <li>
+                <NavLink to="/audio-template">Audio</NavLink>
+              </li>
+              <li>
+                <a href="#">RVM</a>
+              </li>
+              <li>
+                <a href="#">Sales Bridge</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon4} alt="" />
+              </i>
+              <div>
+                <h3>
+                  <NavLink to="/number-list">Number List</NavLink>
+                </h3>
+                <p>Assign number to organization</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="listHead">
+              <i>
+                <img src={SetupIcon5} alt="" />
+              </i>
+              <div>
+                <h3>
+                  <NavLink to="/courses">Courses</NavLink>
+                </h3>
+                <p>Manage your courses</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Setup;
