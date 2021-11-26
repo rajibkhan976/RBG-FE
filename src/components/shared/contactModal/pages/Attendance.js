33272030982f325bdd9913cbebfe81d48b2a36d0
amar-nath-das <amarnath.das@tier5.in>
2021-11-26 14:46:23 +0530

    Contact List attendance UI link

--- src/components/shared/contactModal/pages/Attendance.js
+++ src/components/shared/contactModal/pages/Attendance.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-
 const Attendance = () => {
-
-    return(
-        <>
-            Attendance
-        </>
-    );
-}
+  return (
+    <div className="contactTabsInner">
+      <a className="orangeBtn clockinBtn" href="https://xd.adobe.com/view/1a813aee-7ec1-42ca-9093-051ac3823496-4fd2/screen/e557ffd3-ddb5-4ee1-b6b3-f1b5ff024137/?fullscreen">
+        Attendance Screen
+      </a>
+    </div>
+  );
+};
 
 export default Attendance;
