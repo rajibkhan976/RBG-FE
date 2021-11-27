8e576af99bcc607d43850cc12a6132cb70fd0275
Sourav4525 <sourav_roy@live.com>
2021-11-27 16:48:24 +0530

    contact tab ui fixing

--- src/components/shared/contactModal/pages/Attendance.js
+++ src/components/shared/contactModal/pages/Attendance.js
@@ -3,7 +3,7 @@ import React from "react";
 const Attendance = () => {
   return (
     <div className="contactTabsInner">
-      <a className="orangeBtn clockinBtn" href="https://xd.adobe.com/view/1a813aee-7ec1-42ca-9093-051ac3823496-4fd2/screen/e557ffd3-ddb5-4ee1-b6b3-f1b5ff024137/?fullscreen">
+      <a className="orangeBtn clockinBtn" target="_blank" href="https://xd.adobe.com/view/1a813aee-7ec1-42ca-9093-051ac3823496-4fd2/screen/e557ffd3-ddb5-4ee1-b6b3-f1b5ff024137/?fullscreen">
         Attendance Screen
       </a>
     </div>
