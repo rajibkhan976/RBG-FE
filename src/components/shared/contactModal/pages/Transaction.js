8f1343fe79166b566fd0ffdaa1650147dbe4d50d
jittier5 <jit.talukdar@tier5.in>
2021-12-13 18:51:04 +0530

    Transaction List Fixing

--- src/components/shared/contactModal/pages/Transaction.js
+++ src/components/shared/contactModal/pages/Transaction.js
@@ -3,25 +3,33 @@ import arrow_forward from "../../../../assets/images/arrow_forward.svg";
 import icon_trans from "../../../../assets/images/icon_trans.svg";
 import wwConnect from "../../../../assets/images/wwConnect.svg";
 import wwConnect2 from "../../../../assets/images/wwConnect2.svg";
+import list_board_icon from "../../../../assets/images/list_board_icon.svg";
 
 import { TransactionServices } from "../../../../services/transaction/transactionServices";
 import Loader from "../../Loader";
 
 const Transaction = (props) => {
   const [transactionList, setTransactionList] = useState([]);
+  const [isLoader, setIsLoader] = useState(false);
 
-  const fetchTransactionList = async () => {
-    let transactionResponce = await TransactionServices.fetchTransactionList(
-      props.contactId
-    );
-    setTransactionList(transactionResponce);
+  const fetchTransactionList = async (contactID) => {
+    try {
+      setIsLoader(true);
+      const transactionRes = await TransactionServices.fetchTransactionList(contactID);
+      setTransactionList(transactionRes);
+      console.log("List", transactionRes)
+    } catch (e) {
+
+    } finally {
+      setIsLoader(false);
+    }
   };
   //console.log("transactionList", transactionList);
 
 
 
   useEffect(() => {
-    fetchTransactionList();
+    fetchTransactionList(props.contactId);
   }, []);
 
   const [dt, setDt] = useState(new Date().toLocaleString());
@@ -43,7 +51,7 @@ const Transaction = (props) => {
     //console.log("d1 : ", d1);
     // console.log("d2: ", d2);
     var diff = dt - d1.getTime();
-    
+
     //console.log("diff :: ", diff);
     // var yearCalc =  dt.getFullYear();
     // if(yearCalc % 4 === 0 || yearCalc % 400 === 0 ){
@@ -61,7 +69,7 @@ const Transaction = (props) => {
     var min = modVal / (1000 * 60);
     modVal = modVal % (1000 * 60);
     //var sec = modVal / 1000;
-    
+
     var showTime;
     // if (yeardiff >= 1) {
     //   showTime = Math.floor(yeardiff) + (Math.floor(yeardiff) == 1 ? " year ago" : " years ago");
@@ -69,15 +77,121 @@ const Transaction = (props) => {
     if (daydiff >= 1) {
       showTime = Math.floor(daydiff) + (Math.floor(daydiff) == 1 ? " day ago" : " days ago");
     } else if (Math.floor(hours) > 0) {
-      showTime = Math.floor(hours) + ( Math.floor(hours) == 1 ? " hr ago" : " hrs ago");
+      showTime = Math.floor(hours) + (Math.floor(hours) == 1 ? " hr ago" : " hrs ago");
     } else if (Math.floor(min) >= 1) {
-      showTime = Math.floor(min) + ( Math.floor(min) == 1 ? " min ago" : " mins ago");
+      showTime = Math.floor(min) + (Math.floor(min) == 1 ? " min ago" : " mins ago");
     } else {
       // showTime = Math.floor(min) +  "mins ago";
       showTime = "0 mins ago";
     }
     //console.log(transdate, showTime);
     return showTime;
+  };
+
+  const CourseTransaction = (prop) => {
+    const transData = prop.list;
+    return (
+      <div key={"trans_" + prop.key}
+        className={(transData.outcome.result === "success") ? "row success" : "row fail"}
+      >
+        <div className="cell">
+          <div className="d-flex">
+            <div className="iconCont">
+              <span>
+                <img src={icon_trans} alt="" />
+              </span>
+              {transData.card && (
+                <span className="ifDependent">
+                  <img src={wwConnect} alt="" />
+                </span>
+              )}
+            </div>
+            <div className="textCont">
+              <div className="status">
+                {(transData?.outcome.result) ? transData?.outcome.result : "success"}
+              </div>
+              <div>
+                <span>Course:</span> “
+                {transData.payment_for}”
+              </div>
+              {transData.card && (
+                <a href="" className="dependent">
+                  <span>
+                    <img src={wwConnect2} alt="" />
+                  </span>{" "}
+                  {transData.card.cardholder_name}
+                </a>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="cell">
+          <span
+            className="amount"
+          >
+            {transData.approved_amount
+              ? "$" + transData.approved_amount
+              : "NA"}
+            {/* {transList.is_pos && <span>POS</span>} */}
+          </span>
+        </div>
+        <div className="cell">
+          <span className="transID">
+            {transData.transaction_id
+              ? transData.transaction_id
+              : "NA"}
+            NA
+          </span>
+        </div>
+        <div className="cell">
+          <span className="time">
+            {dateCalculationFunction(transData.transaction_date)}
+          </span>
+        </div>
+      </div>
+    )
+  };
+
+  const PosTransaction = (prop) => {
+    const transData = prop.list;
+    return (
+      <div key={"trans_" + prop.key} className="row success"
+      >
+        <div className="cell">
+          <div className="d-flex">
+            <div className="iconCont">
+              <span>
+                <img src={icon_trans} alt="" />
+              </span>
+            </div>
+            <div className="textCont">
+              <div className="status">
+                Success
+              </div>
+              <div>
+                <span>Product:</span> “
+                {transData.transaction_data.name}”
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="cell">
+          <span className="amount pos">
+            {transData.transaction_data.price}<span>POS</span>
+          </span>
+        </div>
+        <div className="cell">
+          <span className="transID">
+            {transData.transaction_id ? transData.transaction_id : "NA"}
+          </span>
+        </div>
+        <div className="cell">
+          <span className="time">
+            {dateCalculationFunction(transData.transaction_date)}
+          </span>
+        </div>
+      </div>
+    )
   };
 
   return (
@@ -91,6 +205,7 @@ const Transaction = (props) => {
           <span>* Explanatory text blurb should be here.</span>
         </div>
         <div className="transactionListing">
+          {isLoader ? <Loader /> : ''}
           <div className="row head">
             <div className="cell">Particulars</div>
             <div className="cell">Amount</div>
@@ -98,81 +213,18 @@ const Transaction = (props) => {
             <div className="cell">&nbsp;</div>
           </div>
           <>
-            {transactionList.length !== 0 ? (
-              transactionList.transactions.reverse().map((transList, i) => (
-                <>
-                  {/* {console.log("This is", transList )} */}
-                  <div
-                    key={i}
-                    className={
-                      transList.outcome.result === "success"
-                        ? "row success"
-                        : transList.outcome.result === "failed"
-                        ? "row fail"
-                        : "row"
-                    }
-                  >
-                    <div className="cell">
-                      <div className="d-flex">
-                        <div className="iconCont">
-                          <span>
-                            <img src={icon_trans} alt="" />
-                          </span>
-                          {transList.card && (
-                            <span className="ifDependent">
-                              <img src={wwConnect} alt="" />
-                            </span>
-                          )}
-                        </div>
-                        <div className="textCont">
-                          <div className="status">
-                            {transList.outcome.result}
-                          </div>
-                          <div>
-                            <span>{transList.transaction_for}:</span> “
-                            {transList.payment_for}”
-                          </div>
-                          {transList.card && (
-                            <a href="" className="dependent">
-                              <span>
-                                <img src={wwConnect2} alt="" />
-                              </span>{" "}
-                              {transList.card.cardholder_name}
-                            </a>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                    <div className="cell">
-                      <span
-                        className={
-                          transList.is_pos === true ? "amount pos" : "amount"
-                        }
-                      >
-                        {transList.approved_amount
-                          ? "$" + transList.approved_amount
-                          : "NA"}
-                        {transList.is_pos && <span>POS</span>}
-                      </span>
-                    </div>
-                    <div className="cell">
-                      <span className="transID">
-                        {transList.transaction_id
-                          ? transList.transaction_id
-                          : "NA"}
-                      </span>
-                    </div>
-                    <div className="cell">
-                      <span className="time">
-                        {dateCalculationFunction(transList.transaction_date)}
-                      </span>
-                    </div>
-                  </div>
-                </>
-              ))
-            ) : (
-              <Loader />
-            )}
+            {
+              transactionList?.pagination?.count > 0 ? transactionList?.transactions?.map((transaction, key) => {
+                return (!transaction.is_pos) ? <CourseTransaction list={transaction} key={key} /> : <PosTransaction list={transaction} key={key} />;
+              }) : (
+                <div className="createNew">
+                  <span>
+                    <img src={list_board_icon} alt="" />
+                    <p>No transaction found!</p>
+                  </span>
+                </div>
+              )}
+
           </>
         </div>
       </div>
