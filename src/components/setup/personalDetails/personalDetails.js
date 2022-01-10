7020f76a11569f4d3447ccdda7a06ac84cb99355
tier5 <work@tier5.com>
2022-01-10 18:07:09 +0530

    Personal details V0.2

--- src/components/setup/personalDetails/personalDetails.js
+++ src/components/setup/personalDetails/personalDetails.js
@@ -7,6 +7,9 @@ import arrowRightWhite from "../../../assets/images/arrowRightWhite.svg";
 import listIcon from "../../../assets/images/list_icon.svg";
 import dot3gray from "../../../assets/images/dot3gray.svg";
 import edit_gym from "../../../assets/images/edit_gym.svg";
+import arrowDown from "../../../assets/images/arrowDown.svg";
+import profile_img from "../../../assets/images/profile.svg";
+import profile_png from "../../../assets/images/profile.png";
 import gymLogo from "../../../assets/images/gymLogo.svg";
 // import AddHolidayModal from "./addHolidayModal";
 import config from "../../../configuration/config";
@@ -19,18 +22,130 @@ const PersonalDetails = (props) => {
   return (
     <>
     <div className="dashInnerUI">
-        <div class="userListHead">
-          <div class="listInfo">
-            <ul class="listPath">
+        <div className="userListHead detailsPage">
+          <div className="listInfo">
+            <ul className="listPath">
               <li>Settings </li>
               <li> Personal Details</li>
             </ul>
-            <h2 class="inDashboardHeader">
+            <h2 className="inDashboardHeader">
               Personal Details
             </h2>
-            <p class="userListAbout">Lorem ipsum dolor sit amet. Semi headline should be here.</p>
+            <p className="userListAbout">Lorem ipsum dolor sit amet. Semi headline should be here.</p>
           </div>
         </div>
+
+
+
+
+
+
+        <div className="gymDetails personalPage">
+          <div className="gymdetails_left">
+            <h3 className="leftSectionHeader">Account Details</h3>
+              <div className="showing_gym_data">
+                <div className="gymName">
+                  <div className="profilePicture">
+                    <span className="profileImgSection">
+                      <img src={profile_png} className="profileImage" alt="" />
+                      <span>Change</span>
+                    </span>
+                      
+                    <span className="profileName">Jonathan Doe</span>
+                  </div>
+                 
+                  <button><img src={edit_gym} alt=""/></button>
+                </div>
+                <div className="gymInfo full">
+                  <p className="textType1">Email <span>:</span></p>
+                    <p className="textType4">kallol.banerjee@tier5.in</p>
+                </div>
+                <div className="gymInfo full d-flex">
+                  <div className="gymInfo half d-flex">
+                    <p className="textType1">Plan <span>:</span></p>
+                    <p className="textType3">Platinum</p>
+                  </div>
+                  <div className="gymInfo half textRight">
+                  <button class="common_blue_button">Upgrade <img alt="" src={arrowRightWhite} /></button>
+                  </div>
+                </div>
+              </div>  
+          </div>
+
+
+          <div className="gymdetails_right">
+              <div className="holidayListHeader">
+                <h3>Basic Settings</h3>                  
+              </div>
+              <div className="detailsForm">
+                <form>
+                  <div class="formControl">
+                    <label>Timezone</label>
+                    {/* <input type="text" placeholder="Eg. Republic Day" name="" /> */}
+                    <select
+                        style={{
+                            backgroundImage: "url(" + arrowDown + ")",
+                        }}>
+                        <option value="">IST - Asia/Kolkata (GMT+5:30)</option>
+                        <option value="">GST - Europe/England (GMT+0:00)</option>                        
+                    </select>
+                  </div>
+                  <div class="formControl">
+                    <label>Country</label>
+                    {/* <input type="text" placeholder="Eg. Republic Day" name="" /> */}
+                    <select
+                        style={{
+                            backgroundImage: "url(" + arrowDown + ")",
+                        }}>
+                        <option value="">India</option>
+                        <option value="">USA</option>                        
+                    </select>
+                  </div>
+                  <div class="formControl">
+                    <label>Default Number</label>
+                    {/* <input type="text" placeholder="Eg. Republic Day" name="" /> */}
+                    <select
+                        style={{
+                            backgroundImage: "url(" + arrowDown + ")",
+                        }}>
+                        <option value="">+1-2162386318 [ (216) 238-6318 ]</option>
+                        <option value="">+1-2162386318 [ (216) 238-6318 ]</option>                        
+                    </select>
+                  </div>
+                </form>
+              </div>
+
+              <div className="holidayListHeader passwordSection">
+                <h3>Change Password</h3>                  
+              </div>
+              <div className="detailsForm">
+                <form>
+                  <div class="formControl">
+                    <label>Timezone</label>
+                   <input type="text" placeholder="Eg. Republic Day" name="" />
+                  </div>
+                  <div class="formControl">
+                    <label>Country</label>
+                    <input type="text" placeholder="Eg. Republic Day" name="" />
+                  </div>
+                  <div class="formControl">
+                    <label>Default Number</label>
+                    <input type="text" placeholder="Eg. Republic Day" name="" />
+                  </div>
+                  <div class="formControl">
+                  <button type="submit" className="saveNnewBtn"><span>Save</span><img src={arrowRightWhite} alt="" /></button>
+                  </div>
+                </form>
+              </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
     </div>
     </>
   );
