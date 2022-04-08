1a748f8f1ce9da49a787829c1d9505995ac08be1
ishitapaulroy <ishita.paulroy@tier5.in>
2022-04-08 13:14:25 +0530

    fixes after association owner design section

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -177,7 +177,7 @@ const Dashboard = () => {
       </div> : ''} */}
     
       {/* {//original design here.................................................................................................................................} */}
-      {/* <div className="mrrWraper">
+       <div className="mrrWraper">
        
           <h2 className="inDashboardHeader">Dashboard</h2>
           <p className="userListAbout">Get a clear view of your task</p>
@@ -486,10 +486,10 @@ const Dashboard = () => {
          />
          }
          
-        </div>  */}
+        </div>  
 
         {/* {//association owner type1 design here.................................................................................................................................} */}
-           <div className="accosiationDashboard" >
+           {/* <div className="accosiationDashboard" >
             {isLoader ? <Loader /> : ''}
             <div className="contactHead">
                   <div className="userListHead">
@@ -662,7 +662,7 @@ const Dashboard = () => {
                           <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" fill="#305671"></path>
             v           </svg>
                       </button></li></ul></div>
-            </div> 
+            </div>  */}
 
         {/* {//association owner type2 design here.................................................................................................................................} */}
           {/* <div className="accosiationDashboard" >
