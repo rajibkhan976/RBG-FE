8d087e98800eda3e421271eb0e48101a3aa53aaa
ishitapaulroy <ishita.paulroy@tier5.in>
2022-04-08 11:55:49 +0530

    Attendance listing ui fix

--- src/components/report/Attendence/AttendenceListing.js
+++ src/components/report/Attendence/AttendenceListing.js
@@ -44,16 +44,379 @@ const AttendenceListing = (props) => {
     const [permissions, setPermissions] = useState(Object.assign({}, ...JSON.parse(localStorage.getItem("permissions")).filter(el => el.entity === "contact")));
     const dispatch = useDispatch();
     const modalId = useSelector((state) => state.contact.contact_modal_id);
+
+    const [displayState, setDisplayState] = useState("day");
    
 
 
+    useEffect(() => {
+        if (modalId === "") {
+            fetchContact();
+        }
+    }, [modalId]);
+    useEffect(() => {
+        if (!props.modalStatus) {
+            fetchContact();
+        }
+    }, [props.modalStatus]);
+    const handelSize = () => {
+        setTableWidth(window.innerWidth - 504);
+    }
+
+    useEffect(() => {
+        handelSize();
+    }, []);
+
+
+    useEffect(() => {
+        (async () => {
+            const localSavedCol = localStorage.getItem("storedSavedColList", []);
+            const storedSavedColList = (localSavedCol === null) ? [] : JSON.parse(localSavedCol);
+            if (!storedSavedColList.length) {
+                await fetchColumns();
+                await fetchContact();
+            } else {
+                setListCol(storedSavedColList);
+                setSavedColList(storedSavedColList);
+                await fetchContact();
+                // fetchContact();
+            }
+            handleCheckedColListHead();
+            console.log("Stored Saved Column", storedSavedColList);
+        })();
+    }, []);
 
 
 
+    useEffect(() => {
+        if (successMsg) setTimeout(() => { setSuccessMsg("") }, messageDelay);
+        if (errorMsg) setTimeout(() => { setErrorMsg("") }, messageDelay);
+    }, [successMsg, errorMsg])
+
+    const fetchContact = async () => {
+        setIsLoader(true);
+        const pageId = utils.getQueryVariable('page');
+        const queryParams = await getQueryParams();
+        try {
+            const readPermission = (Object.keys(permissions).length) ? await permissions.actions.includes("read") : false;
+            console.clear();
+            console.log(readPermission);
+            if (readPermission === false && env.ACTIVE_PERMISSION_CHECKING === 1) {
+                throw new Error(responses.permissions.automation.read);
+            }
+            const result = await ContactService.fetchUsers(pageId, queryParams);
+            if (result) {
+                console.log("Fetch Contact", result);
+                setContactList(result.contacts);
+                setAppointmentCount(result.pagination.count);
+                setPaginationData({
+                    ...paginationData,
+                    currentPage: result.pagination.currentPage,
+                    totalPages: result.pagination.totalPages
+                });
+                dispatch({
+                    type: actionTypes.CONTACTS_COUNT,
+                    count: result.pagination.count,
+                });
+            }
+        } catch (e) {
+            setErrorMsg(e.message);
+        } finally {
+            setIsLoader(false);
+        }
+    }
+
+    const fetchColumns = async () => {
+        setIsLoader(true);
+        try {
+            const readPermission = (Object.keys(permissions).length) ? await permissions.actions.includes("read") : false;
+            if (readPermission === false && env.ACTIVE_PERMISSION_CHECKING === 1) {
+                throw new Error("");
+            }
+            const result = await ContactService.fetchColumns();
+            if (result) {
+                setListCol(result.columns);
+                setSavedColList(result.columns);
+                localStorage.setItem("storedSavedColList", JSON.stringify(result.columns));
+            }
+        } catch (e) {
+            setErrorMsg(e.message);
+        } finally {
+            setIsLoader(false);
+        }
+    }
+
+    const getQueryParams = async () => {
+        const search = utils.getQueryVariable('search');
+        const group = utils.getQueryVariable('group');
+        const fromDate = utils.getQueryVariable('fromDate');
+        const toDate = utils.getQueryVariable('toDate');
+        const status = utils.getQueryVariable('status');
+        const srtBy = utils.getQueryVariable('sortBy');
+        const srtType = utils.getQueryVariable('sortType');
+
+        const queryParams = new URLSearchParams();
+
+        console.log('search', search)
+        if (search) {
+            queryParams.append("search", search);
+        }
+        if (group) {
+            queryParams.append("group", group);
+        }
+        if (fromDate && toDate) {
+            queryParams.append('fromDate', fromDate);
+            queryParams.append('toDate', toDate);
+        }
+        if (status) {
+            queryParams.append("status", status);
+        }
+        if (srtBy) {
+            queryParams.append("sortBy", srtBy);
+        }
+        if (srtType) {
+            queryParams.append("sortType", srtType);
+        }
+        return queryParams;
+    }
+
+    const handleCheckedColListHead = () => {
+        let dataList = [];
+        let listColData = listCol.filter(el => el.status == true);
+        for (let i = 0; i < listColData.length; i++) {
+            dataList = dataList.concat(listColData[i].id)
+        }
+        setCheckedColListHead(dataList);
+        console.log(dataList);
+    }
+
+    // a little function to help us with reordering the result
+    const reorder = (startIndex, endIndex) => {
+        // const result = Array.from(list);
+        let preSavedCol = listCol;
+        const result = preSavedCol;
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+        return result;
+    };
+
+    const onDragEnd = (result) => {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const item = reorder(
+            result.source.index,
+            result.destination.index
+        );
+
+        // unsavedColList = item;
+        // console.log("unsavedColList", unsavedColList);
+    }
+
+    const handleCheckCol = (e, id) => {
+        console.log(e, id);
+        let data = JSON.parse(JSON.stringify(listCol));
+        let index = data.findIndex(element => element.id == id);
+        console.log(id, index);
+
+        if (index >= 0) {
+            data[index].status = e;
+            setListCol([]);
+            setTimeout(() => setListCol(data), 1);
+            console.log(data);
+        }
+    }
+
+    const handleSave = async () => {
+        setIsLoader(true);
+        try {
+            const res = await ContactService.saveColumns(JSON.stringify({ columns: listCol }));
+            if (res.status) {
+                setSuccessMsg(res.message);
+                localStorage.setItem("storedSavedColList", JSON.stringify(listCol));
+                setSavedColList(listCol);
+                setColModalStatus(false);
+                handleCheckedColListHead();
+                setSearchModalVal("");
+            } else {
+                setErrorMsg("Error in saving the columns. Please try again.");
+            }
+        } catch (e) {
+            setErrorMsg("Error in saving the columns. Please try again.");
+        } finally {
+            setIsLoader(false);
+        }
+    }
+
+    const handleClear = () => {
+        const localSavedCol = localStorage.getItem("storedSavedColList", []);
+        const storedSavedColList = (localSavedCol === null) ? [] : JSON.parse(localSavedCol);
+        setListCol(storedSavedColList);
+        setSavedColList(storedSavedColList);
+        // setColModalStatus(false);
+        setSearchModalVal("");
+
+        // for (let k = 0; k < preSavedCol.length; k++) {
+        //     if (preSavedCol[k].status) {
+        //         listCol[k].status = true;
+        //     } else {
+        //         listCol[k].status = false;
+        //     }
+        // }
+    }
+
+    const handleSortBy = (field) => {
+        // Set sort type
+        let type = "asc"
+        if (field == sortBy) {
+            if (sortType == "asc") {
+                type = "dsc";
+            }
+        }
+        // Set state and Update query param
+        setSortBy(field);
+        setSortType(type);
+        utils.addQueryParameter('sortBy', field);
+        utils.addQueryParameter('sortType', type);
+        // Fetch data
+        // fetchUsers()
+        fetchContact();
+    }
+
+    const handleKeywordChange = (event) => {
+        setKeyword(event.target.value ? event.target.value : '');
+    }
+
+    const handleSearch = (event) => {
+        const readPermission = (Object.keys(permissions).length) ? permissions.actions.includes("read") : false;
+        if (readPermission && env.ACTIVE_PERMISSION_CHECKING === 1) {
+            event.preventDefault();
+            utils.addQueryParameter('page', 1);
+            if (keyword) {
+                utils.addQueryParameter('search', keyword);
+            } else {
+                utils.removeQueryParameter('search');
+            }
+            fetchContact();
+        } else {
+            setErrorMsg(responses.permissions.contact.read);
+        }
+    }
+
+    const GenerateColumns = () => {
+        return (
+            <li className="listHeading">
+                {savedColList.map((item, index) => {
+                    return (
+                        <>
+                            {item.status ? <div className="dataTableCell"
+                                onClick={() => handleSortBy(item.id)}>{item.name}</div> : ""}
+                        </>
+                    )
+                })}
+            </li>
+        )
+    }
+
+    const GenerateContacts = () => {
+        return contactList.map((ele, i) => {
+            let j = 0;
+            return (
+                <li key={'contact_'+i}>
+                    {savedColList.map((item) => {
+                        if (item.status) {
+                            j++;
+                            return (
+                                <div className={item.id === "name" ? "dataTableCell user" : "dataTableCell"}>
+                                    {(j === 1) ? <button className="extraDottedBtn" type="button"></button> : ""}
+                                    <button className="btn" onClick={() => openContactModal(ele._id)}>
+                                        {(item.id === "name") ? <span className="tableCellUserImg">
+                                            <img src={owner_img_1} alt="" />
+                                        </span> : ""}
+                                        {(item.id === 'mobile' || item.id === 'phone' || item.id === 'dadPhone' || item.id === 'momPhone') ?
+                                            ((ele[item.id] && ele[item.id].dailCode &&  ele[item.id].number !== "") ?
+                                                <span className={ele[item.id].is_valid ?
+                                                    "number valid" : "number invalid"}>{ele[item.id].dailCode + "-" + ele[item.id].number}</span> :
+                                                "")  :
+                                            ele[item.id]}
+                                    </button>
+                                </div>
+                            )
+
+                        }
+
+                    })}
+                </li>
+            )
+        })
+    }
+
+    const GenerateColumnDraggableModal = (prop) => {
+        return (
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="dropPlate">
+                    {(provided) => {
+                        return (
+                            <ul ref={provided.innerRef}>
+                                {listCol.filter(el => el.name.toLowerCase().includes(prop.searchData.toLowerCase())).map((item, index) => (
+                                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                                        {(provided) => (
+
+                                            <li
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                            >
+                                                <label>
+                                                    <div className="customCheckbox">
+                                                        <input type="checkbox" onChange={(e) => handleCheckCol(e.target.checked, item.id)} checked={item.status} />
+                                                        <span></span>
+                                                    </div>
+                                                    <span>{item?.name}</span>
+                                                </label>
+                                            </li>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </ul>
+                        )
+                    }}
+                </Droppable>
+            </DragDropContext>
+        );
+    }
+
+    const openContactModal = (id) => {
+        dispatch({
+            type: actionTypes.CONTACTS_MODAL_ID,
+            contact_modal_id: id,
+        });
+    }
+
+    const handleImportModal = () => {
+        const readPermission = (Object.keys(permissions).length) ? permissions.actions.includes("import") : false;
+        if (readPermission && env.ACTIVE_PERMISSION_CHECKING === 1) {
+            props.openModal()
+        } else {
+            setErrorMsg(responses.permissions.contact.import);
+        }
+    }
+
+    
     return (
         <div className="dashInnerUI">
             {isLoader ? <Loader /> : ''}
-            <AttendenceHead></AttendenceHead>
+            <AttendenceHead
+                totalCount={appointmentCount}
+                handleSearch={handleSearch}
+                handleKeywordChange={handleKeywordChange}
+                keyword={keyword}
+                openImportAppointment={handleImportModal}
+                displayState={displayState}
+                setDisplayState={setDisplayState}></AttendenceHead>
             {successMsg &&
                 <SuccessAlert message={successMsg}></SuccessAlert>
             }
@@ -1205,36 +1568,11 @@ const AttendenceListing = (props) => {
             </div>                        
             :''}
             {/* Yearly View Display */}
-            <div class="paginationOuter">
-                <ul>
-                  <li>
-                    <button class="btn paginationBtn">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492 492">
-                        <path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12    C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084    c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864    l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z" fill="#305671"></path>
-                      </svg>
-                    </button>
-                  </li>
-                  <li id="1">
-                    <button class="btn paginationBtn" value="1">1</button>
-                  </li>
-                  <li id="2"><button class="btn paginationBtn" value="2">2</button></li>
-                  <li id="3">
-                      <button class="btn paginationBtn" value="3">3</button>
-                  </li>
-                  <li id="4">
-                    <button class="btn paginationBtn active" value="4">4</button>
-                  </li>
-                  <li id="5">
-                    <button class="btn paginationBtn" value="5">5</button>
-                    </li>
-                   
-                    <li>
-                      <button class="btn paginationBtn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492.004 492.004">
-                          <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" fill="#305671"></path>
-            v           </svg>
-                      </button></li></ul>
-                  </div>
+            {(appointmentCount > paginationData.limit) ? <Pagination
+                type="contact"
+                paginationData={paginationData}
+                dataCount={appointmentCount}
+                callback={fetchContact} /> : ''}
 
         </div>
     );
