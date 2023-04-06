4c37835c1ac0c227f10dcb0123c2e8daf3925f70
sumanmazumdertier5 <suman.mazumder@tier5.in>
2023-04-06 21:36:13 +0530

    notification list time working

--- src/components/shared/Notifications.js
+++ src/components/shared/Notifications.js
@@ -94,12 +94,13 @@ const Notifications = (props) => {
         props.markAllAsRead();
     }
     const showTimeDiff = (e) => {
-        // console.log("Notification date and time", new Date(e.createdAt).toLocaleDateString());
-        let convertTimezone = utils.convertUTCToTimezone(e?.createdAt.trim(), timezoneOffset).split(" ").splice(0,3).join(" ");
+        // let convertTimezone = utils.convertUTCToTimezone(e?.createdAt.trim(), timezoneOffset).split(" ").splice(0,3).join(" ");
+        let convertTimezone = utils.convertUTCToTimezone(e?.createdAt.trim(), timezoneOffset);
+        console.log("Notification date and time", e.createdAt, convertTimezone);
         const dd = String(new Date(convertTimezone).getDate() + 1).padStart(2, "0");
         const mm = String(new Date(convertTimezone).getMonth() + 1).padStart(2, "0");
         const yyyy = new Date(convertTimezone).getFullYear();
-        // console.log("Time zone", yyyy + "-" + mm + "-"+ dd + " " + new Date(convertTimezone).toString().split(" ")[4]);
+        console.log("Time zone", yyyy + "-" + mm + "-"+ dd + " " + new Date(convertTimezone).toString().split(" ")[4]);
         return moment.tz(yyyy+"-"+mm+"-"+dd+" "+ new Date(convertTimezone).toString().split(" ")[4], "Europe/London").fromNow()
     }
     // const modalsStoreCount = useSelector((state) => state.modal.count);
