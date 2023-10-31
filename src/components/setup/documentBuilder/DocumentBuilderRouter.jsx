83913b1534e245400f7967572d61499a8fef77d5
rajibtier5 <rajibhossain.khan@tier5.in>
2023-10-31 17:30:21 +0800

    unnecessary codes are removed

--- src/components/setup/documentBuilder/DocumentBuilderRouter.jsx
+++ src/components/setup/documentBuilder/DocumentBuilderRouter.jsx
@@ -4,18 +4,13 @@ import DashboardFooter from "../../shared/FooterDashboard";
 import DocumentBuilder from "./DocumentBuilder";
 
 const DocumentBuilderRouter = (props) => {
-	const renderID = (randomID) => {
-		console.log("Random ID", randomID);
-	};
 	return (
 		<>
 			<InnerLeftMenu
 				toggleLeftSubMenu={props.toggleLeftSubMenu}
 				routeMenu='setup'
-				reRender={(id) => renderID(id)}
 			/>
 			<div className='dashboardElComponent'>
-				{/* <HeaderDashboard toggleCreate={(e) => props.toggleCreate(e)} /> */}
 				<div className='dashInnerStructure'>
 					<Route
 						path='/document-builder'
